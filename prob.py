f5b0375c2568b349bcc31b21b8af38230e4d3c0c
Pavel <bob-ivanov007@mail.ru>
2019-09-15 16:37:35 +0300

    3.05 na 5000

--- prob.py
+++ prob.py
@@ -34,50 +34,48 @@ for root,dirs,files in os.walk(folder):
         if "RFID.txt" in fil:
             
             if CountryName != root.split("\\")[3]:     
-                cur.execute("SELECT CountryName FROM countries  WHERE countries.CountryName  ='{}'".format(root.split("\\")[3]))
+                cur.execute("SELECT id FROM countries  WHERE countries.CountryName  ='{}'".format(root.split("\\")[3]))
                 data = cur.fetchone()
                 if data != None:
-                    cur.execute("SELECT id FROM countries WHERE countries.CountryName='{}'".format(root.split("\\")[3]))
-                    CountryInfo = cur.fetchone()
-                    Country_id =CountryInfo[0]
+                    #cur.execute("SELECT id FROM countries WHERE countries.CountryName='{}'".format(root.split("\\")[3]))
+                    Country_id =data[0]
                 
                 
                 else :
                     cur.execute('INSERT INTO countries(id,CountryName)  VALUES(?,?)',(None,root.split("\\")[3]))
-                    cur.execute("SELECT id,CountryName FROM countries WHERE countries.CountryName='{}'".format(root.split("\\")[3]))
-                    CountryInfo = cur.fetchall()
-                    
-                    Country_id =CountryInfo[0][0]
-                    CountryName = CountryInfo[0][1]
+                    #cur.execute("SELECT id,CountryName FROM countries WHERE countries.CountryName='{}'".format(root.split("\\")[3]))
+                    #CountryInfo = cur.fetchall()
+                    Country_id +=1
+                    CountryName = root.split("\\")[3]
                     
             if DocumentName != root.split("\\")[4]:
-                cur.execute("SELECT DocumentName FROM TypeOfDocs  WHERE TypeOfDocs.DocumentName  ='{}' AND Country_Ind = '{}'".format(root.split("\\")[4],Country_id))
+                cur.execute("SELECT id FROM TypeOfDocs  WHERE TypeOfDocs.DocumentName  ='{}' AND Country_Ind = '{}'".format(root.split("\\")[4],Country_id))
                 data = cur.fetchone()
                 if data != None:
-                    cur.execute("SELECT id FROM TypeOfDocs WHERE TypeOfDocs.DocumentName='{}' AND Country_Ind = '{}'".format(root.split("\\")[4],Country_id))
-                    DocumentInfo = cur.fetchone()
-                    Document_id =DocumentInfo[0]
+                    #cur.execute("SELECT id FROM TypeOfDocs WHERE TypeOfDocs.DocumentName='{}' AND Country_Ind = '{}'".format(root.split("\\")[4],Country_id))
+                    #DocumentInfo = cur.fetchone()
+                    Document_id =data[0]
                     
                 else:
                     cur.execute('INSERT INTO TypeOfDocs(Country_Ind,id,DocumentName)  VALUES(?,?,?)',(Country_id,None,root.split("\\")[4]))
-                    cur.execute("SELECT id,DocumentName FROM TypeOfDocs WHERE TypeOfDocs.DocumentName='{}' AND Country_Ind = '{}'".format(root.split("\\")[4],Country_id))
-                    DocumentInfo = cur.fetchall()
-                    Document_id =DocumentInfo[0][0]
-                    DocumentName=DocumentInfo[0][1]
+                    #cur.execute("SELECT id,DocumentName FROM TypeOfDocs WHERE TypeOfDocs.DocumentName='{}' AND Country_Ind = '{}'".format(root.split("\\")[4],Country_id))
+                    #DocumentInfo = cur.fetchall()
+                    Document_id +=1
+                    DocumentName=root.split("\\")[4]
                     
             if SampleName != os.path.basename(root):
-                cur.execute("SELECT SampleName FROM samples  WHERE samples.SampleName  ='{}' AND samples.Document_Ind = '{}'".format(os.path.basename(root),Document_id))                          
+                cur.execute("SELECT id FROM samples  WHERE samples.SampleName  ='{}' AND samples.Document_Ind = '{}'".format(os.path.basename(root),Document_id))                          
                 data=cur.fetchone()
                 if data != None:
-                    cur.execute("SELECT id FROM samples WHERE samples.SampleName='{}' AND Document_Ind = '{}'".format(os.path.basename(root),Document_id))
-                    SampleInfo = cur.fetchone()
-                    Sample_id=SampleInfo[0]
+                    #cur.execute("SELECT id FROM samples WHERE samples.SampleName='{}' AND samples.Document_Ind = '{}'".format(os.path.basename(root),Document_id))
+                    #SampleInfo = cur.fetchone()
+                    Sample_id=data[0]
                 else:
                     cur.execute('INSERT INTO samples(id,Document_Ind,SampleName)  VALUES(?,?,?)',(None,Document_id,os.path.basename(root)))
-                    cur.execute("SELECT id,SampleName FROM samples WHERE samples.SampleName ='{}' AND Document_Ind = '{}'".format(os.path.basename(root),Document_id))
-                    SampleInfo = cur.fetchall()
-                    Sample_id =SampleInfo[0][0]
-                    SampleName=SampleInfo[0][1]
+                    #cur.execute("SELECT id,SampleName FROM samples WHERE samples.SampleName ='{}' AND Document_Ind = '{}'".format(os.path.basename(root),Document_id))
+                    #SampleInfo = cur.fetchall()
+                    Sample_id +=1
+                    SampleName=os.path.basename(root)
                                 
 
                         
