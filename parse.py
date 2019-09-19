0595d208b08c64290f45149e11336d025c06cc55
Pavel <bob-ivanov007@mail.ru>
2019-09-19 23:45:41 +0300

    parse_done

--- parse.py
+++ parse.py
@@ -2,13 +2,24 @@ from configparser import ConfigParser
 import os
 folder ='C:\\RD'
 config = ConfigParser()
-numbers=[4,5]
+numbers=(8,9,25)
 for root,dirs,files in os.walk(folder):
     for file in files:
         if "VisualOCR.txt" in file:
-            config.read(os.path.join(root,"VisualOCR.txt"))
+            config.read(os.path.join(root,'VisualOCR.txt'))
+            for i in numbers:
+                k=0
 
+                if config.has_option('Data', '{}'.format(i)) == True:
+                    data=config.get("Data", '{}'.format(i))
+                    fil=open('{0}.txt'.format(i),'a+')
 
+                    fil.seek(0)
+                    for line in fil:
+                        if line == (data+'\n') or line == (data):
+                            k+=1
 
-#config.read("VisualOCR.txt")
-#print(config.has_option('Data', '9'))
+                    if k == 0:
+                         fil.write(data+ '\n')
+                         fil.close()
+
